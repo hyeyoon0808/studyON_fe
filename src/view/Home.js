18bfe7f630d733b6478e74c06a29a3305aeb1129
gpwltl <gpwltl97@naver.com>
2020-10-16 16:07:51 +0900

    add footer

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../assets/scss/Home.scss";
+import Footer from "./Footer";
 
 class Home extends Component {
   render() {
     return (
       <>
-        <div>studyON</div>
-        <Link to="/login">
-          <button>login</button>
-        </Link>
+        {/* header */}
+        <div className="header">
+          <h1>studyON</h1>
+          <div className="button_login">
+            <Link to="/login">
+              <button>login</button>
+            </Link>
+          </div>
+        </div>
+        <div className="contents">contents</div>
+
         <Link to="/room-create">
           <button>create room</button>
         </Link>
         <Link to="/room-list">
           <button>room list</button>
         </Link>
+
+        <Footer />
       </>
     );
   }
