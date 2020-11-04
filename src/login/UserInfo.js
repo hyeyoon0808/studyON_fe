7c9bbc2456d826576b714796be506ef8c96e2e46
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-11-04 15:28:15 +0900

    add UserInfo.js

---
+++ src/login/UserInfo.js
@@ -0,0 +1,12 @@
+const UserInfo = [
+    {
+        msrl: "1",
+        account_id: "1508020350",
+        email: "gpdns@naver.com",
+        name: "조혜윤",
+        password: "",
+        provider: "kakao"
+    }
+];
+
+export default UserInfo;
