2668b3b3b1ad508f5f96162ebfed0b3aca212500
gpwltl <gpwltl97@naver.com>
2020-11-04 12:05:58 +0900

    before pull

--- src/roomList/view/RoomListView.js
+++ src/roomList/view/RoomListView.js
@@ -9,58 +9,58 @@ import tileData from "../tileData";
 import { Link } from "react-router-dom";
 
 const UseStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        overflow: "hidden",
-        backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-        width: "100%",
-        padding: "0 5rem 0 5rem",
-        // height: 450,
-    },
-    icon: {
-        color: "rgba(255, 255, 255, 0.54)",
-    },
-    img: {
-        width: "252px",
-        position: "absolute",
-        top: "-60px"
-    },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: "100%",
+    padding: "0 5rem 0 5rem",
+    // height: 450,
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)",
+  },
+  img: {
+    width: "252px",
+    position: "absolute",
+    top: "-60px",
+  },
 }));
 
 export default function ListView(props) {
-    const { roomList } = props;
-    const classes = UseStyles();
-    return (
-        <div className={classes.root}>
-            <GridList cols={5} cellHeight={180} className={classes.gridList}>
-                {/* <GridListTile key="Subheader"style={{ height: 'auto' }}>
+  const { roomList } = props;
+  const classes = UseStyles();
+  return (
+    <div className={classes.root}>
+      <GridList cols={5} cellHeight={180} className={classes.gridList}>
+        {/* <GridListTile key="Subheader"style={{ height: 'auto' }}>
           <ListSubheader component="div">방리스트</ListSubheader>
         </GridListTile> */}
-                {roomList.length > 0 &&
-                    roomList.map((tile, id) => (
-                        <GridListTile key={tile.id}>
-                            <Link to="/room-entrance">
-                                <img src={tile.img} alt={tile.title} className={classes.img} />
-                            </Link>
-                            <GridListTileBar
-                                title={tile.title}
-                                subtitle={<span>by: {tile.author}</span>}
-                                actionIcon={
-                                    <IconButton
-                                        aria-label={`info about ${tile.title}`}
-                                        className={classes.icon}
-                                    >
-                                        <InfoIcon />
-                                    </IconButton>
-                                }
-                            />
-                        </GridListTile>
-                    ))}
-            </GridList>
-        </div>
-    );
+        {roomList.length > 0 &&
+          roomList.map((tile, id) => (
+            <GridListTile key={tile.id}>
+              <Link to="/room-entrance">
+                <img src={tile.img} alt={tile.title} className={classes.img} />
+              </Link>
+              <GridListTileBar
+                title={tile.title}
+                subtitle={<span>by: {tile.author}</span>}
+                actionIcon={
+                  <IconButton
+                    aria-label={`info about ${tile.title}`}
+                    className={classes.icon}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+      </GridList>
+    </div>
+  );
 }
