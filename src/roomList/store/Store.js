71576e48865377e5ab80d98614b4fb4a73595cec
KKJ620 <kyungju620@gmail.com>
2020-11-05 18:36:44 +0900

    dialog added

--- src/roomList/store/Store.js
+++ src/roomList/store/Store.js
@@ -4,111 +4,142 @@ import { observable, computed, action } from "mobx";
 //import { actionFieldDecorator } from "mobx/lib/internal";
 import RoomApi from "../Api/RoomApi";
 import tagData from "../tagData";
-import tileData from "../tileData";
 
 //1.Mobx Store 클래스 선언
 class Store {
-  roomApi = new RoomApi();
-  //2. 관리해야하는 state 객체 @observable 선언 및 초기화
-  @observable
-  roomName = "";
+    roomApi = new RoomApi();
+    //2. 관리해야하는 state 객체 @observable 선언 및 초기화
+    @observable
+    roomName = "";
 
-  @observable
-  rooms = [];
+    @observable
+    rooms = [];
 
-  @observable
-  user = {};
+    @observable
+    room = {};
 
-  // @observable
-  // tileRooms = tileData;
+    @observable
+    user = {};
 
-  @observable
-  roomName = "";
+    // @observable
+    // tileRooms = tileData;
 
-  @observable
-  tagList = tagData;
+    @observable
+    roomName = "";
 
-  //3. state 데이터 리턴 - @computed get으로 함수 구현
-  // @computed
-  // get getRoomList() {
-  //   return this.roomList ? this.roomList.slice() : [];
-  // }
-  @computed
-  get getRoomList() {
-    return this.rooms ? this.rooms.slice() : [];
-  }
+    @observable
+    tagList = tagData;
 
-  // @computed
-  // get getTileRooms() {
-  //     return this.tileRooms ? this.tileRooms.slice() : [];
-  // }
+    @observable
+    selectedTag = "";
 
-  @computed
-  get getErrorMessage() {
-    return this.errorMessage ? this.errorMessage : "FAIL";
-  }
+    @observable
+    searchList = [];
 
-  @computed
-  get getRoomName() {
-    return this.roomName;
-  }
-  @computed
-  get getUser() {
-    return this.user ? { ...this.user } : {};
-  }
+    //3. state 데이터 리턴 - @computed get으로 함수 구현
+    // @computed
+    // get getRoomList() {
+    //   return this.roomList ? this.roomList.slice() : [];
+    // }
+    @computed
+    get getRoomList() {
+        return this.rooms ? this.rooms.slice() : [];
+    }
 
-  @computed
-  get getTagList() {
-    return this.tagList ? this.tagList.slice() : [];
-  }
+    // @computed
+    // get getTileRooms() {
+    //     return this.tileRooms ? this.tileRooms.slice() : [];
+    // }
 
-  @action
-  setRoomName(roomName) {
-    this.roomName = roomName;
-  }
+    @computed
+    get getErrorMessage() {
+        return this.errorMessage ? this.errorMessage : "FAIL";
+    }
 
-  @action
-  addRoomList() {
-    const tmpData = {
-      img: "./pomodoro.png",
-      title: "테스트 방",
-      author: "테스트",
+    @computed
+    get getRoomName() {
+        return this.roomName;
+    }
+    @computed
+    get getUser() {
+        return this.user ? { ...this.user } : {};
+    }
+
+    @computed
+    get getRoom() {
+        return this.room ? { ...this.room } : {};
+    }
+
+    @computed
+    get getTagList() {
+        return this.tagList ? this.tagList.slice() : [];
+    }
+
+    @computed
+    get getSearchList() {
+        return this.searchList ? this.searchList.slice() : [];
+    }
+
+    @action
+    setRoomName(roomName) {
+        this.roomName = roomName;
+    }
+
+    // @action
+    // addRoomList() {
+    //   const tmpData = {
+    //     img: "./pomodoro.png",
+    //     title: "테스트 방",
+    //     author: "테스트",
+    //   };
+    //   this.roomList = [...this.roomList, tmpData];
+    // }
+
+    @action
+    async roomList() {
+        this.rooms = await this.roomApi.roomList();
+        this.searchList = this.rooms;
+    }
+
+    @action
+    setRoom(owner) {
+        this.room = this.rooms.find((room) => room.owner === owner);
+    }
+
+    @action
+    async userDetail() {
+        this.user = await this.userApi.userDetail();
+    }
+
+    @action
+    findMatches = () => {
+        this.rooms = this.searchList.filter((room) => {
+            // 이 곳에서 검색어와 매치 되는 지를 확인해야 합니다
+            const regex = new RegExp(this.roomName, "gi");
+            return room.title.match(regex);
+        });
     };
-    this.roomList = [...this.roomList, tmpData];
-  }
 
-  @action
-  async roomList() {
-    console.log("roomlist-store");
-    this.rooms = await this.roomApi.roomList();
-    console.log(this.rooms);
-  }
+    // @action
+    // initRoomList = () => {
+    //   this.roomList = this.searchList;
+    // };
 
-  @action
-  async userDetail() {
-    this.user = await this.userApi.userDetail();
-  }
+    @action
+    checkedTagList = (checked, id) => {
+        this.tagList = tagData.map((tag) =>
+            tag.id === id ? { ...tag, checked } : tag
+        );
+        this.selectedTag = this.tagList[id];
+        this.rooms = this.searchList.filter((room) => {
+            return room.tag.includes(this.selectedTag.title);
+            // return room.tag === this.selectedTag.title;
+        });
 
-  @action
-  findMatches = () => {
-    this.tileRooms = tileData.filter((room) => {
-      // 이 곳에서 검색어와 매치 되는 지를 확인해야 합니다
-      const regex = new RegExp(this.roomName, "gi");
-      return room.title.match(regex);
-    });
-  };
-
-  @action
-  initRoomList = () => {
-    this.roomList = tileData;
-  };
-
-  @action
-  checkedTagList = (checked, id) => {
-    this.tagList = tagData.map((tag) =>
-      tag.id === id ? { ...tag, checked } : tag
-    );
-  };
+        if (this.tagList.every((tag) => tag.checked === false)) {
+            this.rooms = this.searchList;
+        }
+    };
 }
 
 //5. 객체 생성해서 export
