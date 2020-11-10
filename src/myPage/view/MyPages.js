d7279f5c6ade53eaaaf126f5a421a79978b34835
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-11-10 23:35:55 +0900

    change router, add currentUser, authenticated

--- src/myPage/view/MyPages.js
+++ src/myPage/view/MyPages.js
@@ -1,44 +1,92 @@
-import React from "react";
+import React, {Component}from "react";
 import "../scss/MyPages.scss";
 import { Card } from "antd";
 import Profile from "../images/Profile.png";
 import MyCalendar from "./MyCalendar";
+import { getCurrentUser } from '../../user/util/APIUtil';
+import { ACCESS_TOKEN } from '../../user/constants';
+import Alert from 'react-s-alert';
 import Header from "../../header/view/Header";
 
-const MyPages = (props) => {
-  // const { user } = props;
-  return (
-    <div className="mypage">
-      <Header />
-      <div className="row">
-        <div className="image">
-          <img class="disabled medium ui image" src={Profile} />
-        </div>
-        <div className="profile">
-          <strong style={{ fontSize: "25px" }}>Profile</strong>
-          <Card className="profileCard">
-            <p>Name: user.name</p>
-            <p>Email: user.email</p>
-            <p>Your Point: </p>
-          </Card>
-        </div>
+class MyPages extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+      currentUser: null,
+      loading: false
+    }
 
-        <div className="row2">
-          <div className="todo">
-            <Card className="todoCard">
-              <strong style={{ fontSize: "18px" }}>Todo List</strong>
-              <p>todo1....</p>
-              <p>todo2....</p>
-              <p>todo3....</p>
+    this.loadCurrentlyLoggedInUser = this.loadCurrentlyLoggedInUser.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  loadCurrentlyLoggedInUser() {
+    this.setState({
+      loading: true
+    });
+
+    getCurrentUser()
+    .then(response => {
+      this.setState({
+        currentUser: response,
+        authenticated: true,
+        loading: false
+      });
+    }).catch(error => {
+      this.setState({
+        loading: false
+      });  
+    });    
+  }
+
+  handleLogout() {
+    localStorage.removeItem(ACCESS_TOKEN);
+    this.setState({
+      authenticated: false,
+      currentUser: null
+    });
+    Alert.success("You're safely logged out!");
+  }
+
+  componentDidMount() {
+    this.loadCurrentlyLoggedInUser();
+  }
+  // const { user } = props;
+  render(){
+    return (
+      <div className="mypage">
+        <Header />
+        <div className="row">
+          <div className="image">
+            <img class="disabled medium ui image" src={Profile} />
+          </div>
+          <div className="profile">
+            <strong style={{ fontSize: "25px" }}>Profile</strong>
+            <Card className="profileCard">
+              <p>Name: name</p>
+              <p>Email: user.email</p>
+              <p>Your Point: </p>
             </Card>
           </div>
-          <div className="calendar">
-            <MyCalendar style={{ width: "100rem" }} />
+
+          <div className="row2">
+            <div className="todo">
+              <Card className="todoCard">
+                <strong style={{ fontSize: "18px" }}>Todo List</strong>
+                <p>todo1....</p>
+                <p>todo2....</p>
+                <p>todo3....</p>
+              </Card>
+            </div>
+            <div className="calendar">
+              <MyCalendar style={{ width: "100rem" }} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default MyPages;
--- src/router/MainRouter.js
+++ src/router/MainRouter.js
@@ -1,17 +1,67 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "../home/view/Home";
 import Login from "../user/view/Login";
 import RoomCreate from "../roomCreate/Container/RoomCreate";
 import RoomList from "../roomList/view/RoomList";
 import Signup from "../user/view/Signup";
+import PrivateRoute from '../myPage/PrivateRoute';
+import { getCurrentUser } from '../user/util/APIUtil';
+import { ACCESS_TOKEN } from '../user/constants';
+import Alert from 'react-s-alert';
 import RoomEntranceView from "../roomEntrance/view/RoomEntranceView";
 import MyPages from "../myPage/view/MyPages";
+import NotFound from "../myPage/NotFound";
+import OAuth2RedirectHandler from '../user/OAuthRedirectHandler';
 import ScrollToTop from "../assets/ScrollToTop";
 import RoomListViewContainer from "../roomList/container/RoomListViewContainer";
 import RoomEntranceContainer from "../roomEntrance/container/RoomEntranceContainer";
 
 class MainRouter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+      currentUser: null,
+      loading: false
+    }
+
+    this.loadCurrentlyLoggedInUser = this.loadCurrentlyLoggedInUser.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  loadCurrentlyLoggedInUser() {
+    this.setState({
+      loading: true
+    });
+
+    getCurrentUser()
+    .then(response => {
+      this.setState({
+        currentUser: response,
+        authenticated: true,
+        loading: false
+      });
+    }).catch(error => {
+      this.setState({
+        loading: false
+      });  
+    });    
+  }
+
+  handleLogout() {
+    localStorage.removeItem(ACCESS_TOKEN);
+    this.setState({
+      authenticated: false,
+      currentUser: null
+    });
+    Alert.success("You're safely logged out!");
+  }
+
+  componentDidMount() {
+    this.loadCurrentlyLoggedInUser();
+  }
+
   render() {
     return (
       <div>
@@ -19,15 +69,20 @@ class MainRouter extends Component {
           {/* <BrowserRouter> */}
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup}/>
+            <Route path="/login" component={Login}
+              render={(props) => <Login authenticated={this.state.authenticated} {...props} />}></Route>
+            <Route path="/signup" component={Signup}
+            render={(props) => <Signup authenticated={this.state.authenticated} {...props} />}>
+            </Route>
             <Route path="/room-create" component={RoomCreate} />
             <Route path="/room-list" component={RoomList} />
             <Route
               path="/room-entrance/:id"
               component={RoomEntranceContainer}
             />
-            <Route path="/myPages" component={MyPages} />
+            <PrivateRoute path="/profile" component={MyPages} authenticated={this.state.authenticated} currentUser={this.state.currentUser}/>
+            <Route path="/oauth2/redirect" component={OAuth2RedirectHandler}></Route> 
+            <Route component={NotFound}></Route>
           </Switch>
           {/* </BrowserRouter> */}
         </ScrollToTop>
