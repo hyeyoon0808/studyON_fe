50df2fd4eae2d8e10229e9d362a76d39806cd11a
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-11-12 11:15:36 +0900

    fixed

---
+++ src/roomCreate/Api/model/RoomApiModel.js
@@ -0,0 +1,64 @@
+import moment from "moment";
+
+class RoomApiModel {
+  userId = 0;
+  title = "기본 제목";
+  description = "기본 공지";
+  startTime = moment().toString();
+  studyTime = "30";
+  breakTime = "5";
+  //currentPeopleNum = 1;
+  maxPeopleNum = 4;
+  tag = "토익";
+  //currentTerm = 1;
+  maxTerm = 6;
+  owner = this.userId;
+
+  constructor(
+    title,
+    description,
+    startTime,
+    studyTime,
+    breakTime,
+    //currentPeopleNum,
+    maxPeopleNum,
+    tag,
+    //currentTerm,
+    maxTerm,
+    owner
+  ) {
+    this.userId = owner;
+    this.title = title;
+    this.description = description;
+    this.startTime = startTime.toString();
+    this.studyTime = studyTime;
+    this.breakTime = breakTime;
+    //this.currentPeopleNum = currentPeopleNum;
+    this.maxPeopleNum = maxPeopleNum;
+    this.tag = tag;
+    //this.currentTerm = currentTerm;
+    this.maxTerm = maxTerm;
+    this.owner = owner;
+  }
+  getJson() {
+    let json = {
+      userId: this.userId,
+      roomInfo: {
+        title: this.title,
+        description: this.description,
+        startTime: this.startTime,
+        studyTime: this.studyTime,
+        breakTime: this.breakTime,
+        //currentPeopleNum: this.currentPeopleNum,
+        maxPeopleNum: this.maxPeopleNum,
+        tag: this.tag,
+        //currentTerm: this.currentTerm,
+        maxTerm: this.maxTerm,
+        owner: this.owner,
+      },
+    };
+    return json;
+  }
+}
+
+export default RoomApiModel;
--- src/roomList/view/RoomListView.js
+++ src/roomList/view/RoomListView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
@@ -7,63 +7,137 @@ import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
 //import { tileData } from "../tileData.json";
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import ButtonTemplate from "../../icon/view/ButtonTemplate";
+import Divider from "@material-ui/core/Divider";
 
 const UseStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: "100%",
-    padding: "0 5rem 0 5rem",
-    // height: 450,
-  },
-  icon: {
-    color: "rgba(255, 255, 255, 0.54)",
-  },
-  img: {
-    width: "252px",
-    position: "absolute",
-    top: "-60px",
-  },
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        overflow: "hidden",
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        width: "100%",
+        padding: "0 5rem 0 5rem",
+        // height: 450,
+    },
+    icon: {
+        color: "rgba(255, 255, 255, 0.54)",
+    },
+    img: {
+        width: "252px",
+        position: "absolute",
+        top: "-60px",
+    },
 }));
 
 export default function RoomListView(props) {
-  const classes = UseStyles();
-  const { rooms } = props;
-  return (
-    <div className={classes.root}>
-      <GridList cols={5} cellHeight={180} className={classes.gridList}>
-        {rooms?.map((tileData) => {
-          return (
-            <GridListTile key={tileData.owner}>
-              <Link to={`/room-entrance/${tileData.owner}`}>
-                {/* <img src={tileData.img} className={classes.img} /> */}
-                <img
-                  src={require("../../home/images/studyon.png")}
-                  alt=""
-                  className={classes.img}
-                />
-              </Link>
-              <GridListTileBar
-                title={tileData.title}
-                subtitle={<span>by: {tileData.author}</span>}
-                actionIcon={
-                  <IconButton
-                    aria-label={`info about ${tileData.title}`}
-                    className={classes.icon}
-                  >
-                    <InfoIcon />
-                  </IconButton>
-                }
-              />
-            </GridListTile>
-          );
-        })}
-      </GridList>
-    </div>
-  );
+    const classes = UseStyles();
+    const { rooms, room, setRoom } = props;
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = (owner) => {
+        setOpen(true);
+        setRoom(owner);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    console.log(rooms);
+    return (
+        <div className={classes.root}>
+            <GridList cols={5} cellHeight={180} className={classes.gridList}>
+                {rooms?.map((room) => {
+                    return (
+                        <GridListTile
+                            key={room.owner}
+                            onClick={() => handleClickOpen(room.owner)}
+                        >
+                            <img
+                                src={require("../../home/images/studyon.png")}
+                                alt=""
+                                className={classes.img}
+                            />
+                            <GridListTileBar
+                                title={room.title}
+                                subtitle={
+                                    <span>
+                                        {" "}
+                                        {room.tag
+                                            ? room.tag
+                                                  .split(" ")
+                                                  .map((tag) => `#${tag} `)
+                                            : null}
+                                    </span>
+                                }
+                                actionIcon={
+                                    <IconButton
+                                        aria-label={`info about ${room.title}`}
+                                        className={classes.icon}
+                                    >
+                                        <InfoIcon />
+                                    </IconButton>
+                                }
+                            />
+                        </GridListTile>
+                    );
+                })}
+            </GridList>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                className="dialog"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {`${room.title}에 입장하시겠습니까?`}
+                </DialogTitle>
+                <DialogContent className="big-input-block">
+                    <div class="input-block">
+                        <span>공부 시작 시간</span>
+                        <span>{room.startTime}</span>
+                    </div>
+                    <div class="input-block">
+                        <span>공부 시간</span>
+                        <span>{room.studyTime} 분</span>
+                    </div>
+                    <div class="input-block">
+                        <span>쉬는 시간</span>
+                        <span>{room.breakTime} 분</span>
+                    </div>
+                    <div class="input-block">
+                        <span>현재 인원</span>
+                        <span>1 바꿔야됨</span>
+                    </div>
+                    <div class="input-block">
+                        <span>최대 인원</span>
+                        <span>{room.maxPeopleNum} 명</span>
+                    </div>
+                    <div class="input-block">
+                        <span>현재 싸이클</span>
+                        <span>1 바꿔야됨</span>
+                    </div>
+                    <div class="input-block">
+                        <span>총 싸이클 횟수</span>
+                        <span>{room.maxTerm} 회</span>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Link to={`/room-entrance/${room.owner}`}>
+                        <ButtonTemplate text={"방 입장"} />
+                    </Link>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
