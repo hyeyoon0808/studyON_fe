cbca9d6c34e02a1a6e19c8c1c00f475fd520e681
ymh254 <ymh254@gmail.com>
2020-11-17 16:05:42 +0900

    add popup

--- src/roomEntrance/view/RoomEntranceView.js
+++ src/roomEntrance/view/RoomEntranceView.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "../scss/RoomEntrance.scss";
 import TabsCard from "./TabsCard";
 import { Card } from "antd";
 import { Checkbox } from "antd";
+import PopUp from "./popup/PopUp"
 // import tileData from "../../roomList/tileData";
 
 function onChange(e) {
@@ -17,6 +18,11 @@ const RoomEntranceView = ({ mySocket, room, rooms, match, owner }) => {
   console.log(params_id);
   console.log(rooms);
   // const data = {};
+
+  const [popUp, setPopUp] = useState(false)
+
+  const duringPopUp = popUp ? "during-popup" : ""
+
   return (
     <>
       <div className="RoomEntrance_wrap" key={data.owner}>
@@ -27,12 +33,17 @@ const RoomEntranceView = ({ mySocket, room, rooms, match, owner }) => {
             <Card title="MY TODO LIST" className="RoomEntrance_left_item">
               <Checkbox onChange={onChange}>todo....</Checkbox>
             </Card>
-            <Card title="Acheivement" className="RoomEntrance_left_item"></Card>
+            <Card title="Acheivement" className="RoomEntrance_left_item">
+              <button onClick={() => setPopUp(true)} className={duringPopUp}>Acheivement</button>
+            </Card>
           </div>
 
           <div className="RoomEntrance_right">
-            <TabsCard roomData={data} mySocket={mySocket} owner={owner} room={room}/>
+            <TabsCard roomData={data} mySocket={mySocket} owner={owner} room={room} />
           </div>
+        </div>
+        <div>
+          {popUp && <PopUp setPopUp={setPopUp} mySocket={mySocket} />}
         </div>
       </div>
     </>
