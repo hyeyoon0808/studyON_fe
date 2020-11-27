8d1d7cac4eda43aaccfee7c5edda5fdc4b8b27f0
gpwltl <gpwltl97@naver.com>
2020-11-27 15:27:56 +0900

    modify roomlist ui, modify achievemnetboard_msg_color

--- src/roomEntrance/view/AchievementBoard.js
+++ src/roomEntrance/view/AchievementBoard.js
@@ -15,73 +15,43 @@ export default function AchievementBoard(props) {
   useEffect(() => {
     console.log(mySocket.id);
     socketRef.current = mySocket;
+
+    //알람 끈 유저 반환
     socketRef.current.on("show user name", (name, count) => {
-      setMessages((oldMsgs) => [
-        ...oldMsgs,
-        name + "가 " + (count + 1) + "번째 알람을 눌렀습니다.",
-      ]);
+      const msg = (
+        <p style={{ color: " #3C36F7", fontFamily: "GmarketSansTTF Medium" }}>
+          {name} 님이 {count + 1} 번째 알람을 눌렀습니다.
+        </p>
+      );
+      setMessages((oldMsgs) => [...oldMsgs, msg]);
     });
+
+    //투두 체크한 유저 반환
     socketRef.current.on("show todo checked", (todoDesc, userName) => {
-      setMessages2((oldTodos) => [
-        ...oldTodos,
-        userName + "가 " + todoDesc + "를 완료했습니다.",
-      ]);
+      const msg = (
+        <p style={{ color: " #4A73D0", fontFamily: "GmarketSansTTF Medium" }}>
+          {userName} 님이 {todoDesc} 를 완료했습니다.
+        </p>
+      );
+      setMessages((oldTodos) => [...oldTodos, msg]);
     });
+
+    //현재 공부 텀 확인
     socketRef.current.on("show the current term", (owner, term) => {
-      setMessages3((oldTerm) => [
-        ...oldTerm,
-        "현재 " + term + " 번째 공부시간 입니다.",
-      ]);
+      const msg = (
+        <p style={{ color: " #00001a", fontFamily: "GmarketSansTTF Medium" }}>
+          쉬는 시간이 끝나면 {term + 1} 번째 공부시간이 시작됩니다.
+        </p>
+      );
+      setMessages((oldTerm) => [...oldTerm, msg]);
     });
   }, []);
 
   return (
     <div className="App">
       <div>
-        {messages3.map((m, index) => {
-          return (
-            //현재 텀 보여주기
-            <div>
-              <p
-                style={{
-                  color: "#00001a",
-                  fontFamily: "GmarketSansTTF Medium",
-                }}
-              >
-                <strong>{m}</strong>
-              </p>
-            </div>
-          );
-        })}
         {messages.map((m, index) => {
-          return (
-            //알람누른 유저 메세지 (유저 & 카운트 갯수)
-            <div>
-              <p
-                style={{
-                  color: "#3C36F7",
-                  fontFamily: "GmarketSansTTF Medium",
-                }}
-              >
-                <strong>{m}</strong>
-              </p>
-            </div>
-          );
-        })}
-        {messages2.map((m, index) => {
-          return (
-            //todo 체크 메시지
-            <div>
-              <p
-                style={{
-                  color: "#4A73D0",
-                  fontFamily: "GmarketSansTTF Medium",
-                }}
-              >
-                <strong>{m}</strong>
-              </p>
-            </div>
-          );
+          return <div>{m}</div>;
         })}
       </div>
     </div>
