2fa1e53f0acc22712a1f5d3c066859a37f4f2484
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-11-30 15:48:13 +0900

    MODIFY uri

--- src/oauth/constants/index.js
+++ src/oauth/constants/index.js
@@ -1,7 +1,7 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://ed51579b-default-springser-29a1-1434439925.us-east-1.elb.amazonaws.com';
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'http://ed51579b-default-springser-29a1-1434439925.us-east-1.elb.amazonaws.com/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
